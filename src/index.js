dd0814a97b135deedd1706286d142fcb96cb26c1
VedantKashettiwar <kvedant164@gmail.com>
2022-10-20 18:19:15 +0530

    by vedant

--- src/index.js
+++ src/index.js
@@ -9,22 +9,12 @@ const port = 8000
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
 
-//functions for fetching data
-function writeFile(text){
-    fs.writeFile('api.txt',text,(error)=>{
-        console.log(error)
-    })
-};
-function checkResponseStatus(res) {
-    if(res.ok){
-        return res
-    } else {
-        throw new Error(`The HTTP status of the reponse: ${res.status} (${res.statusText})`);
-    }
-}
-
 
 const templatePath =path.join(__dirname,'../templates');
+
+let rID = ()=>{
+    return Math.floor(Math.random() * 1000000000000);
+}
 
 //to set the view engine
 app.set('view engine','hbs');
@@ -33,18 +23,28 @@ app.set('views',templatePath)
 app.get('/',(req,res)=>{
     res.render('index')
 })
-app.get('/view',(req,res)=>{
-    res.render('show')
-})
+
 app.post('/',(req,res)=>{
     res.render('msg')
-    const dataObject = req.body
+    let dataObject = {
+        'id':rID(),
+        'fname':req.body.fname,
+        'fphone':req.body.fphone,
+        'femail':req.body.femail
+    }
     const myJSON = JSON.stringify(dataObject);
     let prev = fs.readFileSync('api.json' ,'utf-8')
     prev = prev.slice(0, -1)
     prev =prev+','+myJSON+']'
     fs.writeFileSync('api.json',prev);
 })
+
+app.get('/view',(req,res)=>{
+    let prev = fs.readFileSync('api.json' ,'utf-8')
+    const parseObject = JSON.parse(prev);
+    res.render('show',{parseObject}) 
+})
+
 app.get('*',(req,res)=>{
     res.render('error',{'error':'404, Page not found'})
 })
